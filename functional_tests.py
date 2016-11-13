96dd24f85e1819a6846a19ad1db652b14b30ff97
Joyce Zhu <jzhu29@u.rochester.edu>
2016-11-13 02:06:38 -0500

    Chapter 2 changes

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+       self.browser = webdriver.Firefox()
+       self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_basic(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+        self.fail('Test incomplete')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
